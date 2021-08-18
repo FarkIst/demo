83166fac7513f7911ebe8a1a828990e9ccf2d29c
FarkIst <istvaanfarkas@gmail.com>
2021-08-18 13:29:13 +0200

    Add book entity, controller, repository. Add test

--- src/test/kotlin/com/example/DemoTest.kt
+++ src/test/kotlin/com/example/DemoTest.kt
@@ -1,7 +1,7 @@
 package com.example
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
@@ -11,9 +11,33 @@ class DemoTest {
     @Inject
     lateinit var application: EmbeddedApplication<*>
 
+    @Inject
+    lateinit var bookRepository: BookRepository
+
     @Test
     fun testItWorks() {
-        Assertions.assertTrue(application.isRunning)
+        assertTrue(application.isRunning)
     }
 
+    @Test
+    fun crudTest(){
+        assertNotNull(bookRepository)
+
+        //Create
+        var book = Book(0, "Inferno", "Dante Alighieri's Inferno", 666)
+        bookRepository.save(book)
+
+        val name = book.name
+        assertNotNull(name)
+
+        //Get
+        var newBook = bookRepository.findByName("Inferno")
+        assertNotNull(newBook)
+
+        //Update
+        bookRepository.updateByName("Inferno", "Someone elses Inferno", 96 )
+
+        //Delete
+        bookRepository.delete("Inferno")
+    }
 }
